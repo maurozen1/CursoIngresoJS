6b7b7989dba7aa056f202ee6455eb7719046dadd
maurozen1 <mauro.zenobi1@gmail.com>
2018-07-26 11:23:45 -0300

    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,28 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+        alert("Falta para el invierno");
+    break;
+    case "Junio":
+    case "Julio":
+    case "Agosto":
+     alert("Abrigate que hace frìo")
+    break;
+    default:
+     alert("Ya pasamos el frìo ahora calor!!!") 
+
+
+
+}
+
+
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,6 +3,29 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño)
+{
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":
+        alert("31 días");
+    break;
+    case "Febrero":
+        alert ("29 días");
+    break;
+    default:
+        alert("30 días");
+        
+    
+
+
+}
+
+
 //alert (mesDelAño);
 	
 	
